0d1027f74b1c1ca8592734d9029bf6bc0717ce82
Amey Joshi <74041280+ameyjoshi0209@users.noreply.github.com>
2025-05-08 19:46:19 +0530

    Create data.dart
    - Added API keys for Stripe Payment Gateway
    - Added API keys for Cloudinary Service

---
+++ lib/services/data.dart
@@ -0,0 +1,13 @@
+// KEYS FOR STRIPE PAYMENT GATEWAY
+// Create stripe account and fill the information to activate image handling service in soundstage
+String secretkey =
+    "your-stripe-secret-key-here";
+String publishedkey =
+    "your-stripe-published-key-here";
+
+// PRIVATE DATA FOR CLOUDINARY IMAGE UPLOAD
+// Create cloudinary account and fill the information to activate image handling service in soundstage
+String cloudName = 'cloudinary-cloud-name-here';
+String uploadPreset = 'cloudinary-upload-preset-name-here';
+String apikey = "cloudinary-api-key-here";
+String apiSecretKey = "cloudinary-secret-api-key-here";
